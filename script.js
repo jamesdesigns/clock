d7bc08f383beb8b033e694b72142fda313a2352b
James Hooper <jameshooperdesigns@gmail.com>
2021-01-03 22:06:02 -0700

    Organzie the script between the seconds, minutes, and hours

--- script.js
+++ script.js
@@ -4,19 +4,18 @@ const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
+
     const seconds = now.getSeconds();
-    const minutes = now.getMinutes();
-    const hours = now.getHours();
-
     const secondsDegrees = (( seconds / 60 ) * 360) + 90;
-    const minutesDegrees = (( minutes / 60 ) * 360) + 90;
-    const hoursDegrees = (( hours / 60 ) * 360) + 90;
-
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = (( minutes / 60 ) * 360) + 90;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = (( hours / 60 ) * 360) + 90;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-    
-    console.log(seconds);
 }
 
 setInterval(setDate, 1000);
