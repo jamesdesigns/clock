d5f7cae69b3a3705d79cb717e49a15304a6ac427
James Hooper <jameshooperdesigns@gmail.com>
2021-01-03 22:13:26 -0700

    Adjust the hours math and decrease the clock border

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function setDate() {
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
     const hours = now.getHours();
-    const hoursDegrees = (( hours / 60 ) * 360) + 90;
+    const hoursDegrees = (( hours / 12 ) * 360) + 90;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
